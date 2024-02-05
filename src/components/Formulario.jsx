91c8685f3f4600f168ee4607f4f940bc71d927e7
oliveira-lucas7 <lucasrodrigoclash038@gmail.com>
2024-02-05 17:18:53 -0300

    Mudanças na tela do formulário

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,27 +6,33 @@ function Formulario() {
 
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
     const [checkUm, setCheckUm] = useState(false)
     const [checkDois, setCheckDois] = useState(false)
 
     function sendEmail(e){
-        if(name === '' || email === '' || checkUm === false || checkDois === false){
+        if(name === '' || email === '' || message === '' ||checkUm === false || checkDois === false){
             alert("Você não preencheu todos os campos")
+            e.preventDefault();
             return;
         }
 
         const templateParams = {
             from_name: name,
+            message: message,
             email: email
+            //message: message;
         }
 
-        emailjs.send("service_7cz89cc", "template_wwiab99", templateParams, "6RLZ7eu8T4C1FFQBJ")
+        emailjs.send("service_o350pca", "template_09snx83", templateParams, "6RLZ7eu8T4C1FFQBJ")
         .then((response) => {
             console.log("Email enviado com sucesso", response.status, response.text)
             setName('')
             setEmail('')
+            setMessage('')
             setCheckUm(false)
             setCheckDois(false)
+            e.preventDefault();
         }, (err) => {
             console.log("Erro", err)
         })
@@ -36,7 +42,7 @@ function Formulario() {
     
   return (
     <form onSubmit={sendEmail} className={Style.Formulario}>
-        <h2>Preencha este formuário e receba as promoções em seu email</h2>
+        <h2>Preencha este formuário e tire as suas dúvidas</h2>
         <div>
             <h3>Insira o seu nome</h3>
             <input type="text" 
@@ -50,16 +56,28 @@ function Formulario() {
             <h3>Insira o seu email</h3>
             <input type="email" 
             className={Style.text}
-            name="" 
+            name="email" 
             id="" 
             placeholder='Preencha o seu email neste campo' 
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             />
         </div>
+        <div className={Style.duvida}>
+            <h3>Tire sua dúvida</h3>
+            <textarea className={Style.TextDuvida}
+            maxLength={450}
+            name="message" 
+            id="" 
+            placeholder='Escreva detalhadamente o seu problema ou a sua curiosidade'
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            >
+            </textarea>
+        </div>
         <div className={Style.checkUm}>
             <input type="checkbox" 
-            name="" 
+            name="checkUm" 
             id="" 
             onChange={(e) => setCheckUm(e.target.value)}
             value={checkUm}
@@ -68,7 +86,7 @@ function Formulario() {
         </div>
         <div className={Style.checkDois}>
             <input type="checkbox" 
-            name="" 
+            name="checkDois" 
             id="" 
             onChange={(e) => setCheckDois(e.target.value)}
             value={checkDois}
