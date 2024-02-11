7ddc860d48a860ca177f0c66c09f48fcb1aefe2d
oliveira-lucas7 <lucasrodrigoclash038@gmail.com>
2024-02-11 10:40:48 -0300

    Tela do footer finalizada

--- src/components/Empresa.jsx
+++ src/components/Empresa.jsx
@@ -6,52 +6,52 @@ import { faBook } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 function Empresa() {
-  return (
-    <section className={Style.GeralEmpresa}>
-      <h2 className={Style.SobreEmpresa}>Sobre a empresa</h2>
-      <div className={Style.SobEmpresa}>
-        <div className={Style.ContainerInfo}>
-          <div>
-            <FontAwesomeIcon icon={faBook} className={Style.Icones} />
-            <h3 className={Style.TituloInfo}>Oque é</h3>
-          </div>
-          <div>
-            <p>
-              A A.R.O é uma microempresa que surgiu no ano de 2009, cujo o nosso
-              foco é a venda de induzidos, atendendo o máximo possível de
-              clientes com excelência
-            </p>
-          </div>
-        </div>
-        <div className={Style.ContainerInfo}>
-          <div>
-            <FontAwesomeIcon icon={faKey} className={Style.Icones} />
-            <h3 className={Style.TituloInfo}>Segurança</h3>
-          </div>
-          <div>
-            <p>
-              Temos a garantia de que nossos produtos são de extrema qualidade e
-              segurança, assegurando que os nossos fornecedores estão alinhados
-              com a nossa ética
-            </p>
-          </div>
-        </div>
-        <div className={Style.ContainerInfo}>
-          <div>
-            <FontAwesomeIcon icon={faUser} className={Style.Icones} />
-            <h3 className={Style.TituloInfo}>Atendimento</h3>
-          </div>
-          <div>
-            <p>
-              Temos como objetivo atender e auxiliar todos os nossos cliente da
-              forma mais rápida possível, garantindo que não haja dúvidas
-              naqueles que nos acompanham
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={Style.GeralEmpresa}>
+			<h2 className={Style.SobreEmpresa}>Sobre a empresa</h2>
+			<div className={Style.SobEmpresa}>
+				<div className={Style.ContainerInfo}>
+					<div>
+						<FontAwesomeIcon icon={faBook} className={Style.Icones} />
+						<h3 className={Style.TituloInfo}>Oque é</h3>
+					</div>
+					<div>
+						<p>
+							A A.R.O é uma microempresa que surgiu no ano de 2009, cujo o nosso
+							foco é a venda de induzidos, atendendo o máximo possível de
+							clientes com excelência
+						</p>
+					</div>
+				</div>
+				<div className={Style.ContainerInfo}>
+					<div>
+						<FontAwesomeIcon icon={faKey} className={Style.Icones} />
+						<h3 className={Style.TituloInfo}>Segurança</h3>
+					</div>
+					<div>
+						<p>
+							Temos a garantia de que nossos produtos são de extrema qualidade e
+							segurança, assegurando que os nossos fornecedores estão alinhados
+							com a nossa ética
+						</p>
+					</div>
+				</div>
+				<div className={Style.ContainerInfo}>
+					<div>
+						<FontAwesomeIcon icon={faUser} className={Style.Icones} />
+						<h3 className={Style.TituloInfo}>Atendimento</h3>
+					</div>
+					<div>
+						<p>
+							Temos como objetivo atender e auxiliar todos os nossos cliente da
+							forma mais rápida possível, garantindo que não haja dúvidas
+							naqueles que nos acompanham
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Empresa;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,23 +7,31 @@ function Footer() {
 		<section className={Style.FtGeral}>
 			<div className={Style.IconesGeral}>
 				<div>
-					<a href="">
+					<a href="https://www.instagram.com/lucas_rodrigoo9?igsh=MTdzcTczenI1Zmgycg%3D%3D&utm_source=qr">
 						<FaInstagram className={Style.Instagram} />
 						<h3>lucas_rodrigoo9</h3>
 					</a>
 				</div>
 				<div>
-					<a href="">
+					<a href="https://wa.me/5514997791223">
 						<FaWhatsapp className={Style.Whats} />
 						<h3>Lucas Oliveira</h3>
 					</a>
 				</div>
 				<div>
-					<a href="">
+					<a href="mailto:lucasrodrigoclash038@gmail.com">
 						<FaEnvelope className={Style.Mail} />
 						<h3>lucasrodrigoclash038@gmail.com</h3>
 					</a>
 				</div>
+			</div>
+			<div className={Style.Finalizando}>
+				<span className={Style.Direitos}>
+					© Direitos autorais resevados para a Empresa A.R.O Induzidos
+				</span>
+				<span className={Style.Desenvolvimento}>
+					⚠ Site feito por Lucas Rodrigo de Oliveira
+				</span>
 			</div>
 		</section>
 	);
